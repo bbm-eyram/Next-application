b370e39026f50668a01db9e5245823806079223e
bbm-eyram <kuagbedziephraim@gmail.com>
2025-01-14 12:19:13 +0000

    fixed params

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default async function Product({ params }: { params: {id: string } }){
-    const { id } = await params;
+export default function Product({ params }: { params: { id: string } }) {
+  const { id } = params; // No need to await params since it's not a Promise
+
   return (
     <div>Product: {id}</div>
-  )
+  );
 }
